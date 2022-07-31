c33cd2eb9d75d82036a8b29db902f86b829cc7d3
Ahmed Elgohary <67281513+Ahmed-Elgohary1@users.noreply.github.com>
2022-07-31 15:34:34 +0200

    Update Add Two Numbers.cpp

--- Day 5/Add Two Numbers.cpp
+++ Day 5/Add Two Numbers.cpp
@@ -12,86 +12,33 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
     {
-     int carry=0;
-     ListNode* head=l1,*last1,*last2;   
- 
-        while(l1!=nullptr&&l2!=nullptr)
-        {
-            last1=l1;
-          int temp=(l1->val)+(l2->val)+carry;
-            if(temp>=10)
-            {
-                carry=temp/10;
-                temp%=10;
-            }
-             else
-            carry=0;
-            
-            l1->val=temp;
-            
-           l1= l1->next;
-           l2= l2->next;   
-        }
-        
-        if(l1==NULL&&l2==NULL)
-        {
-            if(carry!=0) 
-       {
-           (last1->next)=new ListNode();
-           (last1->next)->val=carry;
-       } 
-        }
-     
-   if(l2==nullptr)
-    {     
-           
-        while(l1!=NULL)
-        {
-            last1=l1;
-            int temp=(l1->val)+carry;
-            if(temp>=10)
-            {
-                carry=temp/10;
-                temp%=10;
-            }
-             else
-            carry=0;
-            l1->val=temp;
-            
-           l1= l1->next;
-        }
-   if(carry!=0) 
-       {
-           (last1->next)=new ListNode();
-           (last1->next)->val=carry;
-       } 
-    }  
-       
-    if(l1==nullptr)
+       ListNode* ans= new ListNode();
+        ListNode* start=ans;
+        int carry=0;
+    while(l1!=NULL||l2!=NULL)
     {
-      l1=l2;  
-    while(l2!=NULL)
+        int t1=0,t2=0;
+        if(l1!=NULL)
         {
-        last2=l2;
-            int temp=(l2->val)+carry;
-            if(temp>=10)
-            {
-                carry=temp/10;
-                temp%=10;
-            }
-        else
-            carry=0;
-            l2->val=temp;
-            
-           l2= l2->next;
+            t1=l1->val;
+            l1=l1->next;
         }
-      if(carry!=0&&last2) 
-       {
-           (last2->next)= new ListNode();
-           (last2->next)->val=carry;
-       } 
-    }  
+         if(l2!=NULL)
+        {
+            t2=l2->val;
+            l2=l2->next;
+        }
+        ans->next=new ListNode(); ans=ans->next;
+        ans->val=(t1+t2+carry)%10;
+                carry=(t1+t2+carry)/10;
+
+    }
+        if(carry!=0)
+        {
+         ans->next=new ListNode(); ans=ans->next;
+        ans->val=1;   
+        }
          
-   return head;     
+   return start->next;     
     }
 };
