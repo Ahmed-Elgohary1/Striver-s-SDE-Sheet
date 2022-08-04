520c7a5614b796bb998c0d00f3ef0a42fd758166
Ahmed Elgohary <67281513+Ahmed-Elgohary1@users.noreply.github.com>
2022-08-04 12:30:28 +0200

    Create Job Sequencing Problem .cpp

---
+++ Day 8/Job Sequencing Problem .cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+bool sortcol(const vector<int>& v1, const vector<int>& v2)
+{
+    return v1[1] > v2[1];
+}
+
+bool next(int freq[],int n)
+{
+    for(int i=n;i>0;i--)
+    {
+       if(freq[i]==0)
+       {
+         freq[i]=1;
+           return true;
+       }
+    }
+  return false;
+}
+
+int jobScheduling(vector<vector<int>> &jobs)
+{
+    sort(jobs.begin(), jobs.end(), sortcol);
+    int ans=0;
+    int freq[4200]={0};
+    for(int i=0;i<jobs.size();i++)
+    {
+      if(next(freq,jobs[i][0]))
+          ans+=jobs[i][1];
+    }
+    
+        return ans;
+
+}
