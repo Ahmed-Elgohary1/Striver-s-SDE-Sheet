227b3317d5ec857055da3e5e7379447460c215ba
Ahmed Elgohary <67281513+Ahmed-Elgohary1@users.noreply.github.com>
2022-10-07 19:16:05 +0200

    Create Maximum Product Subarray.cpp

---
+++ Dp series/1D Dp/Maximum Product Subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& A) 
+    {
+        int r = A[0];
+
+  
+    for (int i = 1, imax = r, imin = r; i < A.size(); i++) {
+      
+        if (A[i] < 0)
+            swap(imax, imin);
+
+        imax = max(A[i], imax * A[i]);
+        imin = min(A[i], imin * A[i]);
+
+        r = max(r, imax);
+    }
+    return r;
+    }
+};
